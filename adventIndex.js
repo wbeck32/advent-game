5c83f4b1913e63854a7598ff5e028a2602b3a866
Wendy Beck <webeck@gmail.com>
2015-01-14 18:19:32 -0800

    Updates -

--- adventIndex.js
+++ adventIndex.js
@@ -17,13 +17,12 @@ var directionArray = [];
 var initRoom = mazeMap.rooms[0];
 var nextRoom,nextDirection,tmp,index,room;
 
-
 var retrieveDirections = function(roomObj) {
 	directionArray = [];
 	if (roomObj.north !== null) {
 		directionArray.push('north');
 	} if(roomObj.south !== null) {
-		directionArray.push('south')
+		directionArray.push('south');
 	} if (roomObj.west !== null) { 
 		directionArray.push('west');
 	} if (roomObj.east !== null) {
@@ -31,10 +30,22 @@ var retrieveDirections = function(roomObj) {
 	}
 	//remove dupes from array before passing it
 	return directionArray.join(' or ');
-}
+};
 
+
+var findNameObj = function(roomName) {
+	for (room in mazeMap.rooms) {
+		if(mazeMap.rooms[room].name === roomName) {
+			return mazeMap.rooms[room];
+		}
+	}
+};
+var tmp2 = {"rooms":[]};
 var init = function() {
-	console.log(comp.createRoom(initRoom));
+
+	tmp2.rooms.push(initRoom);
+	console.log(require('../jsi-gamelib/').map(tmp2));
+
 	userString = "You are standing in "+initRoom.name;
 	userString += ".\n"+initRoom.description+"\n";
 	userString += "You can go "+retrieveDirections(initRoom)+".";
@@ -45,24 +56,18 @@ var init = function() {
 	});
 };
 
-var findNameObj = function(roomName) {
-	for (room in mazeMap.rooms) {
-		if(mazeMap.rooms[room]['name'] === roomName) {
-			return mazeMap.rooms[room];
-		}
-	}
-}
 	
 var goOn = function(nextDirection, currentRoom) {
-	//console.log();
 	for (index in mazeMap.rooms) {
-		if (mazeMap.rooms[index]['name'] === currentRoom.name){
+		if (mazeMap.rooms[index].name === currentRoom.name){
 			nextRoom = mazeMap.rooms[index][nextDirection];
-		};
-	};
+		}
+	}
 	
 	nextRoom = findNameObj(nextRoom);
-	console.log(comp.createRoom(nextRoom));
+	tmp2.rooms.push(nextRoom);
+
+	console.log(require('../jsi-gamelib/').map(tmp2));
 	userString = "You are standing in "+nextRoom.name;
 	userString += ".\n"+nextRoom.description+"\n";
 	userString += "You can go "+retrieveDirections(nextRoom)+".";
